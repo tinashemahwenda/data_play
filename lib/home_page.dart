eb4c80a0a1c22e5831c953c380cda56194b02126
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-28 12:32:30 +0200

    Finished the basic styling

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,9 +16,20 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Spacer(),
+            Spacer(),
             ShadButton(
               child: Text('Load Data'),
-            )
+              onPressed: () {},
+            ),
+            Spacer(),
+            Text(
+              'API Data',
+              style: ShadTheme.of(context).textTheme.p,
+            ),
+            Spacer(),
+            Spacer(),
+            Spacer()
           ],
         ),
       ),
