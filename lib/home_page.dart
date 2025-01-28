56929426ab8278a131801b20b4106c3ff45a5067
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-28 12:50:54 +0200

    Added more fields

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:shadcn_ui/shadcn_ui.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,6 +12,25 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String data = '';
+  late String type;
+  String punchline = '';
+
+  Future<void> fetchData() async {
+    final response = await http
+        .get(Uri.parse('https://official-joke-api.appspot.com/random_joke'));
+    if (response.statusCode == 200) {
+      setState(() {
+        data = json.decode(response.body)['setup'];
+        type = json.decode(response.body)['type'];
+        punchline = json.decode(response.body)['punchline'];
+        print(data);
+      });
+    } else {
+      print(response);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,12 +41,17 @@ class _HomePageState extends State<HomePage> {
             Spacer(),
             Spacer(),
             ShadButton(
-              child: Text('Load Data'),
-              onPressed: () {},
+              child: Text('Load data'),
+              onPressed: () => fetchData(),
             ),
             Spacer(),
+            ShadBadge(child: Text(type)),
             Text(
-              'API Data',
+              data,
+              style: ShadTheme.of(context).textTheme.p,
+            ),
+            Text(
+              'Punchline: $punchline',
               style: ShadTheme.of(context).textTheme.p,
             ),
             Spacer(),
